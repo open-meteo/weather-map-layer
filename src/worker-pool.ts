dd92a609f6d9a8156c8ddcef1482ac780aa69c94
terraputix <terraputix@mailbox.org>
2025-10-13 23:54:20 +0200

    separate build config for worker

--- src/worker-pool.ts
+++ src/worker-pool.ts
@@ -1,4 +1,4 @@
-import TileWorker from './worker?worker&inline';
+const workerUrl = new URL('./worker/worker.js', import.meta.url);
 
 import type { Data } from './om-protocol';
 
@@ -41,7 +41,7 @@ export class WorkerPool {
 		}
 		const workerCount = navigator.hardwareConcurrency || 4;
 		for (let i = 0; i < workerCount; i++) {
-			const worker = new TileWorker();
+			const worker = new Worker(workerUrl, { type: 'module' });
 			worker.onmessage = (message) => this.handleMessage(message);
 			this.workers.push(worker);
 		}
