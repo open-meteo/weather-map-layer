09d0034496ed660b6342d4a8c48459739ff24ed4
terraputix <terraputix@mailbox.org>
2025-10-13 23:58:18 +0200

    format code

--- vite.worker.config.ts
+++ vite.worker.config.ts
@@ -1,22 +1,22 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  build: {
-    outDir: 'dist/worker',
-    emptyOutDir: false, // so it doesn't wipe your main build
-    lib: {
-      entry: 'src/worker.ts',
-      formats: ['es'],
-      fileName: () => 'worker.js',
-    },
-    rollupOptions: {
-      external: [], // inline everything!
-      output: {
-        entryFileNames: `worker.js`,
-        // This disables code splitting
-        manualChunks: undefined,
-      },
-    },
-    minify: false,
-  }
+	build: {
+		outDir: 'dist/worker',
+		emptyOutDir: false, // so it doesn't wipe your main build
+		lib: {
+			entry: 'src/worker.ts',
+			formats: ['es'],
+			fileName: () => 'worker.js'
+		},
+		rollupOptions: {
+			external: [], // inline everything!
+			output: {
+				entryFileNames: `worker.js`,
+				// This disables code splitting
+				manualChunks: undefined
+			}
+		},
+		minify: false
+	}
 });
