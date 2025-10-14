aead4706989b81e32c61f840118dafa75b677745
Vincent van der Wal <info@vincentvanderwal.nl>
2025-10-14 08:35:45 +0200

    Revert "format code"
    This reverts commit 09d0034496ed660b6342d4a8c48459739ff24ed4.

--- vite.config.ts
+++ vite.config.ts
@@ -3,49 +3,51 @@ import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 
 export default defineConfig({
-	plugins: [dts()],
+	plugins: [
+	  dts(),
+	],
 	optimizeDeps: {
 		exclude: ['@openmeteo/file-reader', '@openmeteo/file-format-wasm']
 	},
 	build: {
 		chunkSizeWarningLimit: 1200,
-		lib: {
-			entry: {
-				index: 'src/index.ts',
-				'worker-pool': 'src/worker-pool.ts',
-				'om-protocol': 'src/om-protocol.ts',
-				types: 'src/types.ts',
-				'utils/arrow': 'src/utils/arrow.ts',
-				'utils/color-scales': 'src/utils/color-scales.ts',
-				'utils/domains': 'src/utils/domains.ts',
-				'utils/icons': 'src/utils/icons.ts',
-				'utils/interpolations': 'src/utils/interpolations.ts',
-				'utils/math': 'src/utils/math.ts',
-				'utils/projections': 'src/utils/projections.ts',
-				'utils/variables': 'src/utils/variables.ts'
-			},
-			formats: ['es']
-		},
+	  lib: {
+      entry: {
+        index: 'src/index.ts',
+        'worker-pool': 'src/worker-pool.ts',
+        'om-protocol': 'src/om-protocol.ts',
+        types: 'src/types.ts',
+        'utils/arrow': 'src/utils/arrow.ts',
+        'utils/color-scales': 'src/utils/color-scales.ts',
+        'utils/domains': 'src/utils/domains.ts',
+        'utils/icons': 'src/utils/icons.ts',
+        'utils/interpolations': 'src/utils/interpolations.ts',
+        'utils/math': 'src/utils/math.ts',
+        'utils/projections': 'src/utils/projections.ts',
+        'utils/variables': 'src/utils/variables.ts',
+      },
+      formats: ['es'],
+    },
 		rollupOptions: {
-			external: ['@openmeteo/file-reader', '@openmeteo/file-format-wasm'],
+		  external: ['@openmeteo/file-reader', '@openmeteo/file-format-wasm'],
 			output: {
 				entryFileNames: `[name].js`,
 				chunkFileNames: `[name].js`,
-				assetFileNames: `[name].[ext]`
+				assetFileNames: `[name].[ext]`,
 				// name: 'mapbox-layer',
 				// format: "esm",
 				// inlineDynamicImports: true
 			},
-			preserveEntrySignatures: 'strict'
+			preserveEntrySignatures: 'strict',
 		},
 		minify: false
 	},
-	worker: {
-		format: 'es',
-		rollupOptions: {
-			output: {
-				entryFileNames: `[name].js`
+	 worker:{
+			format: 'es',
+			rollupOptions: {
+				output: {
+					entryFileNames: `[name].js`,
+				}
 			}
-		}
-	}
+	 }
 });
