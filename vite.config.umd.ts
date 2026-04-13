7cb5451e8735a20d39e71eea34fb2b847260c24e
Terraputix <terraputix@mailbox.org>
2026-04-13 10:37:05 +0200

    fix: vite 8 type generation (#228)

--- vite.config.umd.ts
+++ vite.config.umd.ts
@@ -9,7 +9,7 @@ export default defineConfig({
 			formats: ['umd'],
 			fileName: (_format) => `index.js`
 		},
-		rollupOptions: {
+		rolldownOptions: {
 			output: {
 				globals: {
 					'@openmeteo/file-reader': 'OpenMeteoFileReader',
