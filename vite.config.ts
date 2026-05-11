3292fb539d46452aca6844aa0143bdd11b0cf5d2
Vincent van der Wal <111272374+vincentvdwal@users.noreply.github.com>
2026-05-11 10:54:04 +0200

    fix: update to rolldown vite v8 and ts v6 (#246)

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import dts from 'unplugin-dts/rollup';
+import dts from 'unplugin-dts/rolldown';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
@@ -14,7 +14,7 @@ export default defineConfig({
 	},
 	build: {
 		chunkSizeWarningLimit: 1200,
-		rollupOptions: {
+		rolldownOptions: {
 			external: ['@openmeteo/file-reader', '@openmeteo/file-format-wasm'],
 			input: {
 				index: 'src/index.ts'
